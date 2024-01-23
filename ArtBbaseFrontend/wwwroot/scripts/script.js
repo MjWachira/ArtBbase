4469caeb81551c83f5b7251260730793a1ca817f
Johnny <mjwachira1@gmail.com>
2024-01-23 09:56:54 +0300

    Auth Service created

---
+++ ArtBbaseFrontend/wwwroot/scripts/script.js
@@ -0,0 +1,29 @@
+window.ShowMessage = (type, message) => {
+    if (type === 'success') {
+        Swal.fire({
+            title: "Added Successfully!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type === 'error') {
+        Swal.fire({
+            title: "Deleted!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type === 'edit') {
+        toastr.success(message)
+    }
+
+    if (type === 'login') {
+        toastr.success(message)
+    }
+
+
+
+}
+function toggleSidebar(displayValue) {
+    document.getElementById("sidebar").style.display = displayValue;
+}
